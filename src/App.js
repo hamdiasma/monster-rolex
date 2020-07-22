6f8c5608e3295c17b94b460feb40041076de99bf
hamdiasma <hamdi.babdelhafidh@gmail.com>
2020-07-22 18:12:15 +0100

    fetch api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-     
-          Learn React
-    
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      monstsers: [],
+    };
+  }
+  componentDidMount = () => {
+    fetch("http://jsonplaceholder.typicode.com/users").then((response) =>
+      response.json()
+    ).then(users=> this.setState({monstsers:users}))
+  };
+  render() {
+    console.log(this.state.monstsers)
+    return (
+      <div className="App">
+        {this.state.monstsers.map((monster) => (
+          <p key={monster.id}>{monster.name}</p>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
