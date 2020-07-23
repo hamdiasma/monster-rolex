7134c922c1f70183fdd7d040f92ae7f6f1bf012c
hamdiasma <hamdi.babdelhafidh@gmail.com>
2020-07-23 12:07:29 +0100

    fixed css

--- src/components/card-list/card.jsx
+++ src/components/card-list/card.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import "./card-list.css";
 const Card = ({ monster }) => {
   return (
-    <div className="col-3 mt-3">
-      <div className="card" style={{ width: "200px" ,height:"300px"}}>
+    <div className="col-lg-3 col-md-4  col-sm-6 col-xs-12  text-center ">
+      <div
+        className="card   mt-3 mx-auto"
+        style={{ width: "200px", height: "300px" }}
+      >
         <img src="..." className="card-img-top" alt="..." />
         <div className="card-body">
           <p className="card-text">{monster.name}</p>
